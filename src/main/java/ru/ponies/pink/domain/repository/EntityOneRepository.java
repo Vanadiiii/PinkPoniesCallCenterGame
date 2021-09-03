4ad09e58216ed36d964245e72671f2bdcee2b72e
Ivan Matveev <ivan.matveev@vsegda-da.com>
2021-09-04 02:48:06 +0400

    [no-task] remove camunda's stuff

--- src/main/java/ru/ponies/pink/domain/repository/EntityOneRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.ponies.pink.domain.repository;
-
-import ru.ponies.pink.domain.entity.EntityOne;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface EntityOneRepository extends JpaRepository<EntityOne, UUID> {
-}
--- src/main/java/ru/ponies/pink/domain/repository/EntityThreeRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.ponies.pink.domain.repository;
-
-import ru.ponies.pink.domain.entity.EntityThree;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface EntityThreeRepository extends JpaRepository<EntityThree, UUID> {
-}

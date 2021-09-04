d778e1e6a4ca45f4044cf4c8320300b5b0805c27
neliseev <Nikita.Eliseev@vsegda-da.com>
2021-09-04 04:39:43 +0400

    [no-task] small fix

--- src/main/java/ru/ponies/pink/web/dto/MetricsDto.java
+++ src/main/java/ru/ponies/pink/web/dto/MetricsDto.java
@@ -1,14 +1,15 @@
 package ru.ponies.pink.web.dto;
 
 import lombok.Data;
-import org.springframework.jmx.support.MetricType;
+import ru.ponies.pink.domain.entity.enums.SubjectType;
 
+import java.math.BigDecimal;
 import java.util.Map;
 import java.util.UUID;
 
 @Data
 public class MetricsDto {
     private UUID subjectId;
-    private MetricType type;
-    private Map<String, String> metrics;
+    private SubjectType type;
+    private Map<String, BigDecimal> metrics;
 }
--- src/main/java/ru/ponies/pink/web/mapper/UserDtoMapper.java
+++ src/main/java/ru/ponies/pink/web/mapper/UserDtoMapper.java
@@ -1,12 +1,11 @@
 package ru.ponies.pink.web.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
 import ru.ponies.pink.domain.entity.User;
 import ru.ponies.pink.web.dto.UserDto;
 
 import java.util.function.Function;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
+@Mapper(componentModel = "spring")
 public interface UserDtoMapper extends Function<UserDto, User> {
 }

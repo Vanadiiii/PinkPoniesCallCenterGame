92161477b4e1f364480abc67dcb744e0a823ed05
Novozhilov Konstantin <kostyansa@gmail.com>
2021-09-04 16:09:02 +0400

    [no-task] added endpoint for recommendations

---
+++ src/main/java/ru/ponies/pink/web/RecomendationSystemController.java
@@ -0,0 +1,32 @@
+package ru.ponies.pink.web;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import ru.ponies.pink.domain.entity.Strategy;
+import ru.ponies.pink.service.RecommendationSystem;
+import ru.ponies.pink.service.mapper.StrategyDtoMapper;
+import ru.ponies.pink.web.dto.StrategyDto;
+
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/recommendation")
+@PreAuthorize("isAuthenticated()")
+public class RecomendationSystemController {
+
+    final private RecommendationSystem recommendationSystem;
+
+    @GetMapping
+    public ResponseEntity<Strategy> create(@NotNull @RequestBody UUID id) {
+
+        return ResponseEntity.ok(recommendationSystem.recommend(id));
+    }
+}
